7faca564c43cbb9cb779b525f2f8983eab7cb83f
Simran Bardhan <simranbardhan13@gmail.com>
2023-08-11 16:00:22 +0530

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Simran Bardhan on 09/08/23.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
